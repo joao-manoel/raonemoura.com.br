b11cae1d1af8c3afcc8b220375945d214f0a795f
João Manoel <joaomanoel.ms.br@gmail.com>
2022-02-16 10:54:37 -0400

    facebook pixel added

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from "react";
 import Document, { DocumentContext, Html, Main, Head, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+import {Pixel} from '../components/pixel'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -35,9 +37,8 @@ class MyDocument extends Document {
       <Html lang="pt_Br">
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="description" content="Junte-se ao telegram da semana do violino!" />
-
-          
+          <meta name="description" content="Junte-se ao telegram da semana do violino!" /> 
+          <Pixel />         
         </Head>
         <body>
           <Main />
