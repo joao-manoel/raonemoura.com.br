a7ace7f6cf4fca7293045ec6853e8302fc882d32
João Manoel <joaomanoel.ms.br@gmail.com>
2022-02-16 11:03:12 -0400

    pixel udpate

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -2,8 +2,6 @@ import React, { ReactElement } from "react";
 import Document, { DocumentContext, Html, Main, Head, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-import {Pixel} from '../components/pixel'
-
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -38,7 +36,21 @@ class MyDocument extends Document {
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="description" content="Junte-se ao telegram da semana do violino!" /> 
-          <Pixel />         
+          <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2134823353331816');
+            fbq('track', 'PageView');`}} />
+            
+      <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=2134823353331816&ev=PageView&noscript=1"
+        />` }}
+        />      
         </Head>
         <body>
           <Main />
